947685e97487399579cda7b2f5deb993b4bf4dc7
Carlos Yu <1109122780@qq.com>
2022-09-15 20:54:50 -0700

    merge-3

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!GameIsPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Resume();
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Loading menu");
+        Application.Quit();
+    }
+
+    // IEnumerator Wait()
+    // {
+    //     yield return new WaitForSeconds(3);
+
+    // }
+}
