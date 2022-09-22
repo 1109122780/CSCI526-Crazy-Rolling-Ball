c61a95e0f2702ed3a3c3066e26d8b19cc732a037
Carlos Yu <1109122780@qq.com>
2022-09-21 17:36:54 -0700

    debug

---
+++ Assets/Scripts/Destory.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destory : MonoBehaviour
+{
+    public GameObject destroy;
+    public GameObject lose;
+    double change = 2;
+    // public  int ballSize;
+    // Start is called before the first frame update
+
+    void OnCollisionEnter(Collision other)
+    {
+        var size = transform.GetComponent<Collider>().bounds.size;
+        if (other.gameObject.tag == "Wall" )
+        {
+            if(size.x > 0.5 * change)
+            {
+                Instantiate(destroy, transform.position, Quaternion.identity);
+                Destroy(other.gameObject);
+            }
+            else
+            {
+                lose.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
+    }
+}
