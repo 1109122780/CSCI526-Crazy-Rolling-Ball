6dc6f4c3282af821dbdfbd68a4f956ae4f0dfdba
dragonlzm <liuzhuom@usc.edu>
2022-09-24 16:21:32 -0700

    update for the main menu bottom

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public string firstLevel;
+    public string secondLevel;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,16 +22,12 @@ public class MainMenu : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene(firstLevel);
+        //SceneManager.LoadScene(0);
     }
 
-    public void OpenOptions()
+    public void StartTutorial()
     {
-
-    }
-
-    public void CloseOptions()
-    {
-
+        SceneManager.LoadScene(secondLevel);
     }
 
     public void QuitGame()
