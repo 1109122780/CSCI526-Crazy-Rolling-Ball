e692b0b7f357e4e6424a54698bf1cd08f68f6e1c
martin9d <tianyanding@gmail.com>
2022-10-11 12:46:31 -0700

    feature4

--- Assets/Scripts/Break.cs
+++ Assets/Scripts/Break.cs
@@ -6,6 +6,7 @@ public class Break : MonoBehaviour
 {
     private GameObject destroy;
     private GameObject player;
+    private bool high = false;
     double change = 2;
     // Start is called before the first frame update
     void Start()
@@ -15,14 +16,20 @@ public class Break : MonoBehaviour
     }
 
     // Update is called once per frame
-    
+    void Update(){
+        if (player.transform.localPosition.y > 2 && player.transform.localScale.x >1)
+        {
+            high = true;
+        }
+    }
     void OnCollisionEnter(Collision other)
     {
         var size = other.transform.GetComponent<Collider>().bounds.size;
-        if (other.gameObject.tag == "Player" && size.x > 0.5 * change)
+        if (other.gameObject.tag == "Player" && size.x > 0.5 * change && high)
         {
             Instantiate(destroy, other.transform.position, Quaternion.identity);
             Destroy(transform.gameObject);
         }
+        
     }
 }
--- Assets/Scripts/Destory.cs
+++ Assets/Scripts/Destory.cs
@@ -6,50 +6,23 @@ public class Destory : MonoBehaviour
 {
     private GameObject destroy;
     private GameObject player;
-    private GameObject checkPoint;
-    public GameObject lose;
     double change = 2;
-    // public  int ballSize;
     // Start is called before the first frame update
-    void Start(){
+    void Start()
+    {
         destroy = GameObject.Find("ColliderDestroy");
         player = GameObject.Find("Player");
     }
 
-    void OnTriggerEnter(Collider other)
+    // Update is called once per frame
+    
+    void OnCollisionEnter(Collision other)
     {
-        // var size = transform.GetComponent<Collider>().bounds.size;
         var size = other.transform.GetComponent<Collider>().bounds.size;
-        if (other.gameObject.tag == "Player" )
+        if (other.gameObject.tag == "Player" && size.x > 0.5 * change)
         {
-            if(size.x > 0.5 * change)
-            {
-                Instantiate(destroy, other.transform.position, Quaternion.identity);
-                Destroy(transform.gameObject);
-            }
-            else
-            {
-                if (other.name == "Player")
-                {
-                    if (player.GetComponent<PlayerMovement>().health > 0)
-                    {
-                        player.GetComponent<PlayerMovement>().health --;
-                        if (player.GetComponent<PlayerMovement>().hasCheckPoint)
-                        {
-                            player.transform.position = player.GetComponent<PlayerMovement>().checkPoint.GetComponent<Transform>().position;
-                        }
-                        else
-                        {
-                           player.transform.position = new Vector3(0, 1, 0);
-                        }
-                    }
-                    else
-                    {
-                        lose.SetActive(true);
-                        Time.timeScale = 0;
-                    }
-                }
-            }
+            Instantiate(destroy, other.transform.position, Quaternion.identity);
+            Destroy(transform.gameObject);
         }
     }
 }
