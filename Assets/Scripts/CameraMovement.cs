9c71948ef8b64ee96d16f27f217530c8a19a610f
dragonlzm <liuzhuom@usc.edu>
2022-10-11 15:10:27 -0700

    update for the camera angle movement  v1
    the camera angle logic change to: when pressing the mouse and moving mouse at the same time, we can change the camera angle. The function is implemented in the CameraMovement.cs

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,6 +6,7 @@ public class CameraMovement : MonoBehaviour
 {
     public GameObject Target;
     public float smooth = 2f;
+    public float Speed = 5;
     Vector3 distance;
     // Start is called before the first frame update
     void Start()
@@ -13,10 +14,24 @@ public class CameraMovement : MonoBehaviour
         distance = transform.position - Target.transform.position;
     }
  
-    // Update is called once per frame
-    void LateUpdate()
+    //Update is called once per frame
+    // void LateUpdate()
+    // {
+    //     transform.position = Vector3.Lerp(Target.transform.position+distance, transform.position, Time.deltaTime * smooth);
+    //     transform.LookAt(Target.transform.position);//摄像头Wink物体，不然不丝滑
+    // }
+
+    // use update function to change the camera angle and the position
+    void Update()
     {
         transform.position = Vector3.Lerp(Target.transform.position+distance, transform.position, Time.deltaTime * smooth);
-        transform.LookAt(Target.transform.position);//摄像头Wink物体，不然不丝滑
+        // change the camera angle when press the mouse and move the mouse
+        if(Input.GetMouseButton(0))
+        {
+            transform.eulerAngles += Speed * new Vector3(x: -Input.GetAxis("Mouse Y"), y: Input.GetAxis("Mouse X"), z: 0);
+        }
+        //transform.LookAt(Target.transform.position);
     }
+
+
 }
