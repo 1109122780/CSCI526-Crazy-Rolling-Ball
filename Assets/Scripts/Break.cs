de42ec8776a9d33430d5f56cee701e505956f247
martin9d <tianyanding@gmail.com>
2022-10-10 23:43:37 -0700

    feature4

---
+++ Assets/Scripts/Break.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Break : MonoBehaviour
+{
+    private GameObject destroy;
+    private GameObject player;
+    double change = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        destroy = GameObject.Find("ColliderDestroy");
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    
+    void OnCollisionEnter(Collision other)
+    {
+        var size = other.transform.GetComponent<Collider>().bounds.size;
+        if (other.gameObject.tag == "Player" && size.x > 0.5 * change)
+        {
+            Instantiate(destroy, other.transform.position, Quaternion.identity);
+            Destroy(transform.gameObject);
+        }
+    }
+}
--- Library/Bee/artifacts/emscripten/f6a1j/wrapper_emcc.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-"E:\3.8f1\Editor\Data\PlaybackEngines\WebGLSupport\BuildTools\Emscripten\emscripten\emcc.bat" %* < nul
-exit %ERRORLEVEL%
