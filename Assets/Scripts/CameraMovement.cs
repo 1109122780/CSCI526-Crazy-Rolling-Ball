eeff19be8a799cc7a6e62e204a61da753a04824c
dragonlzm <liuzhuom@usc.edu>
2022-10-11 22:39:05 -0700

    update for camera movement v2

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,33 +5,25 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public GameObject Target;
-    public float smooth = 2f;
-    public float Speed = 5;
-    Vector3 distance;
+    Vector3 finalOffset;
+
     // Start is called before the first frame update
     void Start()
     {
-        distance = transform.position - Target.transform.position;
+        finalOffset = transform.position - Target.transform.position;
     }
  
     //Update is called once per frame
-    // void LateUpdate()
-    // {
-    //     transform.position = Vector3.Lerp(Target.transform.position+distance, transform.position, Time.deltaTime * smooth);
-    //     transform.LookAt(Target.transform.position);//摄像头Wink物体，不然不丝滑
-    // }
-
-    // use update function to change the camera angle and the position
-    void Update()
+    void LateUpdate()
     {
-        transform.position = Vector3.Lerp(Target.transform.position+distance, transform.position, Time.deltaTime * smooth);
-        // change the camera angle when press the mouse and move the mouse
-        if(Input.GetMouseButton(0))
-        {
-            transform.eulerAngles += Speed * new Vector3(x: -Input.GetAxis("Mouse Y"), y: Input.GetAxis("Mouse X"), z: 0);
-        }
-        //transform.LookAt(Target.transform.position);
+        Rotate();
+        transform.position = Vector3.Lerp(transform.position, (Target.transform.position + finalOffset), .25f);
+        // make the camera alway look at the player.
+        transform.LookAt(Target.transform.position);
     }
-
-
+    // calculate the rotation
+    void Rotate()
+    {
+        finalOffset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * 4f, Vector3.up) * finalOffset;
+    }
 }
