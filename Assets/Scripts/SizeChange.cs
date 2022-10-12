00228cf07064198ae0af85f7560b3fb1dcae5012
samuelusc <97491762+samuelusc@users.noreply.github.com>
2022-10-11 17:02:03 -0700

    update Size change
    1. update size change speed and animation
    2. Delate CD time
    3. update jump when player become smaller

--- Assets/Scripts/SizeChange.cs
+++ Assets/Scripts/SizeChange.cs
@@ -11,124 +11,144 @@ public class SizeChange : MonoBehaviour
     private PlayerMovement jump_script;
     public GameObject information;
 
+    private float sizeFloat = 0.005f;
+    private float positonFloat = 0.005f;
+
+    private bool sizeUp = false;
+    private bool sizeDown = false;
+
+    public bool jumpHigh;
 
     // Start is called before the first frame update
     void Start()
     {
-        v = new Vector3(0, 1, 0);
-        sizeCool = true;
-
-        jump_script = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
-        information = GameObject.Find("Information");
-
+                v = new Vector3(0, 1, 0);
+                // sizeCool = true;
+                jumpHigh = false;
+                jump_script = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+                information = GameObject.Find("Information");
 
     }
 
     // Update is called once per frame
     void Update()
-    {
-
-        if (Input.GetKeyDown(KeyCode.Q) & sizeCool & (jump_script.worldTag == 1 || jump_script.inTest))
-        {
-            information.GetComponent<InformationScript>().times_QE++;
-            if (!jump_script.isgrounded)
-            {
-                return;
-            }
-            if (Time.timeScale == 0)
-            {
-                sizeCool = true;
-                return;
-            }
-            v = transform.localPosition;
-
-            if (transform.localScale.y > 1.0f)
-            {
-                v.y = 1;
-                transform.localPosition = v;
-                transform.localScale = new Vector3(1, 1, 1);
-                sizeCool = false;
-                jumpLock = false;
-                StartCoroutine(CoolDown());
-
-            }
-            else
-            {
-                StopAllCoroutines();
-                v.y = 2;
-                transform.localPosition = v;
-                transform.localScale = new Vector3(3, 3, 3);
-
-                jumpLock = true;
-                StartCoroutine(FiveSeconds());
-            }
-
-        }
-        if (Input.GetKeyDown(KeyCode.E) & sizeCool & (jump_script.worldTag == 1 || jump_script.inTest))
-        {
+    {     
+        if (Input.GetKeyDown(KeyCode.Q) & (jump_script.worldTag == 1 || jump_script.inTest)) {
             // information.GetComponent<InformationScript>().times_QE++;
-            if (!jump_script.isgrounded)
-            {
+            if(!jump_script.isgrounded) {
                 return;
             }
-            if (Time.timeScale == 0)
-            {
-                sizeCool = true;
+            if(Time.timeScale == 0) {
+                // sizeCool = true;
+                jumpHigh = false;
+                return;
+            }
+            v = transform.localPosition; 
+
+            if(transform.localScale.y > 1.0f){
+            v.y = 1;
+            transform.localPosition = v;
+            transform.localScale = new Vector3(1, 1, 1);
+            // sizeCool = false;
+            jumpLock = false;
+            jumpHigh = false;
+            // StartCoroutine(CoolDown());
+            
+            }
+            else 
+            {   
+                StopAllCoroutines();
+                sizeUp = true;
+                jumpLock = true;
+                jumpHigh = false;
+                // StartCoroutine(FiveSeconds());
+            }
+                
+        }
+        if (sizeUp & transform.localScale.y < 3.0f) {
+            if(sizeDown) {
+                sizeDown = false;
+            }
+            sizeFloat = 0.006f;
+            positonFloat = 0.006f;
+            // jumpLock = true;
+            v.y += positonFloat;
+            transform.localPosition = v;
+            transform.localScale += new Vector3(sizeFloat, sizeFloat, sizeFloat);
+        }
+        else {
+            sizeUp = false;
+            // jumpLock = false;
+            StartCoroutine(FiveSeconds());
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) & (jump_script.worldTag == 1 || jump_script.inTest)) {
+            // information.GetComponent<InformationScript>().times_QE++;
+            if(!jump_script.isgrounded) {
+                return;
+            }
+            if(Time.timeScale == 0) {
+                // sizeCool = true;
+                jumpHigh = false;
                 return;
             }
             v = transform.localPosition;
-            if (transform.localScale.y < 1.0f)
+            if(transform.localScale.y < 1.0f)
             {
-                transform.localScale = new Vector3(1, 1, 1);
-                v.y = 1;
-                transform.localPosition = v;
-
-                jumpLock = false;
-                sizeCool = false;
-                StartCoroutine(CoolDown());
-                // StopCoroutine(FiveSeconds());
+            transform.localScale = new Vector3(1, 1, 1);
+            v.y = 1;
+            transform.localPosition = v;
+            jumpHigh = false;
+            jumpLock = false;
             }
-            else
-            {
+            else 
+            {   
                 StopAllCoroutines();
-                v.y = 0.8f;
-                transform.localPosition = v;
-                transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-
-                // small ball can't jump:
-                // jumpLock = false;
-                jumpLock = true;
+                sizeDown = true;
+                jumpLock = false;
                 // sizeCool = true;
                 StartCoroutine(FiveSeconds());
             }
-
+                
         }
-        timer = 5.0f;
+        if (sizeDown & transform.localScale.y > 0.5f){ 
+            if(sizeUp) {
+                sizeUp = false;
+            }
+            positonFloat = 0.001f;
+            v.y -= positonFloat;
+            transform.localPosition = v;
+            transform.localScale -= new Vector3(sizeFloat,sizeFloat,sizeFloat);
+        }
+        else {
+            sizeDown = false;
+            jumpHigh = true;
+            StartCoroutine(FiveSeconds());
+        }
+        timer = 5.0f; 
     }
-    private IEnumerator FiveSeconds()
-    {
+    private IEnumerator FiveSeconds() {
         // var v = transform.localPosition;
-        while (timer > 0)
-        {
+        while (timer > 0) {
             yield return new WaitForSeconds(1000.0f);
             v = transform.localPosition;
             v.y = 1;
             transform.localPosition = v;
             transform.localScale = new Vector3(1, 1, 1);
             timer -= 5.0f;
-            sizeCool = false;
+            // sizeCool = false;
             jumpLock = false;
 
             yield return new WaitForSeconds(2.0f);
-            sizeCool = true;
+            // sizeCool = true;
             StopAllCoroutines();
-
+        
         }
     }
-    private IEnumerator CoolDown()
-    {
-        yield return new WaitForSeconds(2.0f);
-        sizeCool = true;
-        StopAllCoroutines();
-    }
+    // private IEnumerator CoolDown() {
+    //     yield return new WaitForSeconds(2.0f);
+    //     sizeCool = true;
+    //     StopAllCoroutines();
+    // }
+    
 }
