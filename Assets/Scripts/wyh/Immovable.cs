80e55aca9d30dcdb4f2df66ce15c574e6f6b93a2
Yuhui Wu <winslow0440@gmail.com>
2022-11-02 23:26:27 -0700

    Fix 2-1 ball can move box bug

---
+++ Assets/Scripts/wyh/Immovable.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Immovable : MonoBehaviour
+{
+    private GameObject sphere;
+    private GameObject cube;
+    float lightMass = 10;
+    float heavyMass = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Collide Player");
+            for (int i = 0; i < collision.gameObject.transform.childCount; i++)
+            {
+                if (collision.gameObject.transform.GetChild(i).name == "PlayerSphere")
+                {
+                    sphere = collision.gameObject.transform.GetChild(i).gameObject;
+                }
+
+                if (collision.gameObject.transform.GetChild(i).name == "PlayerCube")
+                {
+                    cube = collision.gameObject.transform.GetChild(i).gameObject;
+                }
+            }
+
+            if (cube.activeInHierarchy)
+            {
+                
+                gameObject.GetComponent<Rigidbody>().mass = lightMass;
+                Debug.Log("The mass is:" + gameObject.GetComponent<Rigidbody>().mass);
+            } 
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            if (cube.activeInHierarchy)
+            {
+                gameObject.GetComponent<Rigidbody>().mass = heavyMass;
+                Debug.Log("The mass is:" + gameObject.GetComponent<Rigidbody>().mass);
+            }
+        }
+    }
+
+}
